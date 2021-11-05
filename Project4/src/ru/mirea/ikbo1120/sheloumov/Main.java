cbae4451e0b47547eee0cc6fda48e6a519af423d
Diverbriz <79372098+Diverbriz@users.noreply.github.com>
2021-11-05 19:48:17 +0300

    Project4

---
+++ Project4/src/ru/mirea/ikbo1120/sheloumov/Main.java
@@ -0,0 +1,14 @@
+package ru.mirea.ikbo1120.sheloumov;
+
+import ru.mirea.ikbo1120.sheloumov.Classes.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        new FootballScore();
+    }
+}
